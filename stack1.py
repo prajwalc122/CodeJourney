292431413232f4750c8407549120c9ae3a6f19a0
Prajwal <prajwal9625@gmail.com>
2026-04-26 19:08:47 +0530

    Add files via upload

---
+++ stack1.py
@@ -0,0 +1,9 @@
+stack=[]
+
+#append the value towards the stack
+stack.append("prajwal")
+stack.append("ninja")
+stack.append("version")
+
+# after append the value towards the stack will be 
+print("After pushing value to the stack is :",stack)
