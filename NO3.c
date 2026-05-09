e05bb41a7517f8aed61ba0077023ece834db9726
Prajwal <prajwal9625@gmail.com>
2026-05-09 22:19:38 +0530

    Add files via upload

---
+++ NO3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int m, n, i, j;
+    printf("Enter the m student marks:");
+    scanf("%d",&m);
+    printf("Enter the n student marks:");
+    scanf("%d",&n);
+    printf("Enter the i student marks:");
+    scanf("%d",&i);
+    printf("Enter the j student marks:");
+    scanf("%d",&j);
+    
+    printf("Complete marks Is below:\n");
+    
+    printf("%d\n",m);
+    printf("%d\n",n);
+    printf("%d\n",i);
+    printf("%d\n",j);
+}
