79d3774beeadbe9ef877397ef8df144741acfb9e
Prajwal <prajwal9625@gmail.com>
2026-05-11 22:03:57 +0530

    Add files via upload

---
+++ ArithmeticDemo.java
@@ -0,0 +1,19 @@
+class ArithmeticDemo{
+    public static void main(String args[]) {
+
+        try {
+            int a = 10;
+            int b = 0;
+
+            int c = a / b;
+
+            System.out.println(c);
+        }
+
+        catch(ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+
+        System.out.println("Program ended");
+    }
+}
