4e4977d857d036e9ff8d3d39e31e446fdac6fe65
Prajwal <prajwal9625@gmail.com>
2026-07-09 16:18:09 +0530

    Refactor conditions for odd and even checks

--- h2.py
+++ h2.py
@@ -1,11 +1,21 @@
+# Program starts from here
 if __name__ == '__main__':
+
+    # Take input from the user and convert it to an integer
     n = int(input().strip())
 
+    # Check if the number is odd
     if n % 2 != 0:
         print("Weird")
-    elif n % 2 == 0 and 2 <= n <= 5:
+
+    # Check if the number is even and between 2 and 5
+    elif 2 <= n <= 5:
         print("Not Weird")
-    elif n % 2 == 0 and 6 <= n <= 20:
+
+    # Check if the number is even and between 6 and 20
+    elif 6 <= n <= 20:
         print("Weird")
+
+    # If the number is even and greater than 20
     else:
         print("Not Weird")
