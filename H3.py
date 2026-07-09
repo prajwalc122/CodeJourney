d18ea41381a0151db30b1aa0c8849d26f74f0e11
Prajwal <prajwal9625@gmail.com>
2026-07-09 16:22:39 +0530

    Add number classification script in H3.py

---
+++ H3.py
@@ -0,0 +1,14 @@
+# Take a number as input
+num = int(input("Enter a number: "))
+
+# Check if the number is positive
+if num > 0:
+    print("Positive Number")
+
+# Check if the number is negative
+elif num < 0:
+    print("Negative Number")
+
+# If neither positive nor negative, it is zero
+else:
+    print("Zero")
