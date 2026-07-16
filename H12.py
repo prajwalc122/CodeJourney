164ee9a20e9b6f25063b6c521125f97d6c38f55c
Prajwal <prajwal9625@gmail.com>
2026-07-16 21:45:31 +0530

    Create H12.py
    Expanded the numbers list and corrected comment syntax.

---
+++ H12.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40,50,60,70,999,777,666,444,445,667,45,0990,123,344]
+
+
+#removes 20
+numbers.remove(20)  
+
+numbers.pop()    
+# removes last element
+del numbers[1] 
+# delete by index
