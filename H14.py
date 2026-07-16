15f5da68c2b79b0fab116bd399383f8804849cd1
Prajwal <prajwal9625@gmail.com>
2026-07-16 21:47:55 +0530

    Update print statements for clarity in H14.py

---
+++ H14.py
@@ -0,0 +1,8 @@
+numbers = [5, 2, 9, 1]
+
+print(len(numbers))    # show the  Length of an numbers.
+print(max(numbers))    # Find the Max value
+print(min(numbers))    # Find the Min value
+
+numbers.sort()         # Sort ascending
+numbers.reverse()      # Reverse list
