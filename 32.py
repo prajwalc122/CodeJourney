3eb0c1dc8cbbad2a13b4a6af42ca4cd8e6a7bfdf
Prajwal <prajwal9625@gmail.com>
2026-07-24 08:32:52 +0530

    Implement array_sum function to calculate sum

---
+++ 32.py
@@ -0,0 +1,16 @@
+//def named function calls than gave function name called 
+array sum than give s=0 because s value will be zero
+than usinfg for loop to iteration give x variable name 
+and arr ,
+and s+=x means S=s+x so we 
+x=10 adda with s and as its iterates finally using return keyword
+to give result.
+
+
+
+arr=[10,20,30,40,50]
+def array_sum(arr):
+    s = 0
+    for x in arr:
+        s += x
+    return s
