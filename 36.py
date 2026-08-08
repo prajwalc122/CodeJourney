6d6d992949474cd15cd12b59f4e22976584ccbe8
Prajwal <prajwal9625@gmail.com>
2026-08-08 22:26:11 +0530

    Add for loop tutorial in 36.py
    Added a tutorial on using for loops in Python.

---
+++ 36.py
@@ -0,0 +1,18 @@
+'''1. for Loop
+Use a for loop when you want to iterate through a sequence or a known range of values.
+Basic syntax
+for variable in sequence:
+    # code
+Example:
+for i in range(5):
+    print(i)
+Output:
+0
+1
+2
+3
+4
+Notice something important:
+range(5) gives:
+0 1 2 3 4
+It doesn't include 5.'''
