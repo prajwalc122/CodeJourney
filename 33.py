e85d2e2c4c4b9c46a2a13b9144bd5f6c4f2cba63
Prajwal <prajwal9625@gmail.com>
2026-08-08 22:23:30 +0530

    Create 33.py with Python loops explanation
    Added a Python script demonstrating loops with examples.

---
+++ 33.py
@@ -0,0 +1,21 @@
+'''Python Loops — Complete Concept
+A loop is used when we want to execute the same block of code repeatedly.
+For example, instead of writing:
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+we can use a loop:
+for i in range(1, 6):
+    print(i)
+Output:
+1
+2
+3
+4
+5
+There are mainly two loops in Python:
+for loop
+while loop
+'''
