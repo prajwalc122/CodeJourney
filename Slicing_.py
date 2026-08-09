d1ec180c8d481422a5ea48281a54c87af24dd8ce
Prajwal <prajwal9625@gmail.com>
2026-08-09 19:06:11 +0530

    Add slicing examples in Slicing_.py

---
+++ Slicing_.py
@@ -0,0 +1,61 @@
+
+text = "PROGRAMMING"
+print(text[-3:])
+
+texts="PYTHON"
+print(texts[-3:])
+
+text="HELLO"
+print(text[1:])
+
+text="PYTHON"
+print(text[0:5])
+
+text="COMPUTER"
+print(text[2:5])
+
+text="PROGRAMMING"
+print(text[2:6])
+
+text="JAVAscript"
+print(text[-4:])
+
+text="python"
+print(text[-4:])
+
+text="COMPUTER"
+print(text[ :-2])
+
+text="PYTHON"
+print(text[-4:-1])
+
+text="PYTHON"
+print(text[::2])
+
+
+text="PYTHON"
+print(text[1::2])
+
+text = "PROGRAMMING"
+print(text[1::3])
+
+text="PYTHON"
+print(text[::-1])
+
+text = "PROGRAMMING"
+print(text[6:1:-1])
+
+email = "prajwal123@gmail.com"
+print(email[:10])
+
+filename = "python_notes.pdf"
+print(filename[-3:])
+
+date = "09-08-2026"
+
+print("Day:", date[:2])
+print("Month:", date[4:5])
+print("Year:", date[-4:])
+
+text = "Python is easy"
+print(text[::-1])
