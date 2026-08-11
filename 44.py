e28f2ffb1ba8c0e793cede660a9c4bb571655394
Prajwal <prajwal9625@gmail.com>
2026-08-11 22:17:24 +0530

    Implement palindrome check for an array
    Add a function to check if an array is a palindrome.

---
+++ 44.py
@@ -0,0 +1,20 @@
+arr = [1, 2, 3, 2, 1]
+
+left = 0
+right = len(arr) - 1
+
+is_palindrome = True
+
+while left < right:
+
+    if arr[left] != arr[right]:
+        is_palindrome = False
+        break
+
+    left += 1
+    right -= 1
+
+if is_palindrome:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
