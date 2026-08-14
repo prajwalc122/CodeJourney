3e4023a03ffc68ed86b5df8c67c68da3a7176353
Prajwal <prajwal9625@gmail.com>
2026-08-14 21:43:39 +0530

    Add program to find the largest of three numbers

---
+++ 51.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+if a >= b and a >= c:
+    print("A is largest")
+elif b >= a and b >= c:
+    print("B is largest")
+else:
+    print("C is largest")
